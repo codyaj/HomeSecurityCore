5d7cacb59eac4211cc9385e3aae17256bf935ddb
Cody Jackson <57662320+codyaj@users.noreply.github.com>
2025-05-04 04:18:25 +1000

    Create config.h

---
+++ Alarm/include/config.h
@@ -0,0 +1,15 @@
+// Home Security Core – Proof of Concept
+// © 2025 Cody Jackson. All rights reserved.
+// This file is part of the Home Security Lite release.
+// Intended for educational and demonstration purposes only. Not for commercial use.
+// DISCLAIMER: This is a work-in-progress. Some features are incomplete or not yet implemented.
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+
+#define ALARM_PIN D8
+#define MAX_NONCE 20
+
+
+#endif
